eb83ce80b25538e3f659935537e3189455e36de7
Navinazhagu <145822175+Navinazhagu@users.noreply.github.com>
2024-08-27 13:07:40 +0530

    Add files via upload

---
+++ dem01/Dwm01Application.java
@@ -0,0 +1,13 @@
+package com.ex2.dwm01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dwm01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dwm01Application.class, args);
+	}
+
+}
---
+++ dem01/Repository.java
@@ -0,0 +1,5 @@
+package com.ex2.dem01;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Repository extends JpaRepository<StudModel,Integer> {
+}
